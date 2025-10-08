b9f26c23c9b3f7b8b63e451ff19cde29c91ca570
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-08 19:52:12 +0530

    Added another condition

---
+++ conditions5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "enter any number'
+read Num
+if [ $Num is -gt 100 ]
+then
+   echo "your input number $Num is greaterthan 100"
+else
+   echo "your input number $Num is lessthan 100"
+fi   
+
