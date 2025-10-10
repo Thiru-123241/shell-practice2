a947999b8aff583c7335182d3effec3e6b238dbd
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-10 23:37:08 +0530

    Added exitstatus

---
+++ exitstatus11.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Name=Thirumalesh
+Age=28
+Company="Tech Mahindra"
+Education=Btech
+course=ece
+
+echo "$Company: Techmahindra"
+echo "$Education: BE"
+echo "$course: electronics and communications
+exit 120
